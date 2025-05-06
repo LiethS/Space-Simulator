592d731503981879596aabe70a81882c70f70e7d
LiethS <Lieth.munther@gmail.com>
2025-05-06 03:54:54 -0400

    rendering Fixes & Small Tweaks

--- engine/entities.py
+++ engine/entities.py
@@ -1,6 +1,5 @@
 # Holds classes of generic entities
 
-import math
 from config import GRAVITY_CONSTANT
 class Entity:
     def __init__(self, position, velocity, mass, radius, color):
@@ -17,38 +16,6 @@ class Entity:
 
     def reset_force(self):
         self.force = [0.0, 0.0]
-
-    def update(self, dt):
-        # F = ma -> a = F/m
-        ax = self.force[0] / self.mass
-        ay = self.force[1] / self.mass
-
-        # Integrate velocity
-        self.velocity[0] += ax * dt
-        self.velocity[1] += ay * dt
-
-        # Integrate position
-        self.position[0] += self.velocity[0] * dt
-        self.position[1] += self.velocity[1] * dt
-
-        self.reset_force()
-
-    def compute_gravitational_force(self, other):
-        dx = other.position[0] - self.position[0]
-        dy = other.position[1] - self.position[1]
-        dist_sq = dx**2 + dy**2
-        dist = math.sqrt(dist_sq)
-
-        if dist == 0:
-            return  # Avoid division by zero
-
-        force_magnitude = GRAVITY_CONSTANT * self.mass * other.mass / dist_sq
-        fx = force_magnitude * dx / dist
-        fy = force_magnitude * dy / dist
-
-        self.apply_force(fx, fy)
-        other.apply_force(-fx, -fy)
-
 
 class Planet(Entity):
     def __init__(self, name, position, velocity, mass, radius, color):
